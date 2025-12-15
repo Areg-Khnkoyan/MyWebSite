57dbd58470e4eb9e28b4dcea7c23185d918072b3
Areg-Khnkoyan <88333043+Areg-Khnkoyan@users.noreply.github.com>
2025-12-15 21:49:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,12 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
